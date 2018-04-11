f9512209331d1e737b3d98a251e26722d97d5b8c
Brandon Yang <200151@hkis.edu.hk>
2018-04-11 22:16:22 +0800

    wef

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-function hello(){
-  document.getElementById("soundtext").innerHTML = "hi";
+function entry1(){
+	if (document.getElementById('entryOne').style.backgroundColor == "none") {
+		var firstEntry = prompt("")
+		document.getElementById('entryOne').style.backgroundColor == "green"}
+    }
+	else {
+		document.getElementById("journaltext") = firstEntry
+	}
+	
 }
 
