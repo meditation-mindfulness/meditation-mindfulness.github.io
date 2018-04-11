d0282c151891f1fafbe2f8cfab65035b1946361b
Brandon Yang <200151@hkis.edu.hk>
2018-04-11 22:10:52 +0800

    regerg

--- script.js
+++ script.js
@@ -1,13 +1,4 @@
-var date = new Date();
-localStorage.setItem("soundclick", 0)
-
-
-function statistics(){
-	document.getElementById("time").innerHTML = date; 
-	}
-
-function sounds(){
-	localStorage.soundclick = Number(localStorage.soundclick) + 1;
-
-	alert(soundclicks)
+function hello(){
+  document.getElementById("soundtext").innerHTML = "hi";
 }
+
